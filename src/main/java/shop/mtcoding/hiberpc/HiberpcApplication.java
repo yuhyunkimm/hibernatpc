ce2296eb7e77fc35dcbd782b793d3dd78c1807db
YouHyunKim <yuhyunk7@gmail.com>
2023-03-17 10:16:47 +0900

    yml setting

---
+++ src/main/java/shop/mtcoding/hiberpc/HiberpcApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.hiberpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiberpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/hiberpc/HiberpcApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.hiberpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
