5edae170f6a57fd8d5d6450d76d0f69b2a1d76c0
YouHyunKim <yuhyunk7@gmail.com>
2023-03-17 11:42:17 +0900

    board 생성

--- src/main/java/shop/mtcoding/hiberpc/model/board/Board.java
+++ src/main/java/shop/mtcoding/hiberpc/model/board/Board.java
@@ -1,5 +1,36 @@
 package shop.mtcoding.hiberpc.model.board;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import shop.mtcoding.hiberpc.model.user.User;
+
+@NoArgsConstructor // 없으면 하이버네이트가 뉴를 못함
+@Getter
+@Setter
+@Entity
+@Table(name = "board_tb")
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id; // 포린키는 n에 붙혀준다
+    // private Integer userId; // ORM
+    @ManyToOne // 앞에는 클래스 / Many = Board , One = User
+    private User user; // 모순 되는것을 하이버네이트가 해결해 준다
+    private String title;
+    private String content;
+    @CreationTimestamp
+    private Timestamp createdAt;
 
 }
