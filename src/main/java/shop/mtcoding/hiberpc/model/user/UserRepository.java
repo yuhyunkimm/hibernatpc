b225a7c616c21949deec0a393c77edf4ee99033e
YouHyunKim <yuhyunk7@gmail.com>
2023-03-17 11:49:09 +0900

    userepository 생성

--- src/main/java/shop/mtcoding/hiberpc/model/user/UserRepository.java
+++ src/main/java/shop/mtcoding/hiberpc/model/user/UserRepository.java
@@ -1,8 +1,39 @@
 package shop.mtcoding.hiberpc.model.user;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Repository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Repository
 public class UserRepository {
+
+    private final EntityManager em;
+
+    public User findById(int id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() { // User Class아님 객체
+        return em.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    public User save(User user) {
+
+        if (user.getId() == null) {
+            em.persist(user);
+        } else {
+            em.merge(user);
+        }
+        return user;
+    }
+
+    public void delete(User user) {
+        em.remove(em);
+    }
 
 }
