b4d52345d3bf314d03bf06aa8e204e4576c01ac5
YouHyunKim <yuhyunk7@gmail.com>
2023-03-17 12:07:08 +0900

    userepository 수정반영

--- src/main/java/shop/mtcoding/hiberpc/model/user/UserRepository.java
+++ src/main/java/shop/mtcoding/hiberpc/model/user/UserRepository.java
@@ -22,12 +22,17 @@ public class UserRepository {
         return em.createQuery("select u from User u", User.class).getResultList();
     }
 
-    public User save(User user) {
+    public User save(User user) { // 서비스에서 검증을 하던 
 
         if (user.getId() == null) {
             em.persist(user);
         } else {
-            em.merge(user);
+           User userPS = em.find(User.class, user.getId())
+           if (userPS == null) {
+               em.merge(user);
+           } else {
+                System.out.println("잘못된 머지를 하셨습니다");
+           }
         }
         return user;
     }
